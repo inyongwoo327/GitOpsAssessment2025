3e98ceb2ff0236a168221172a3c782dec4f64b9c
Evan Woo <inyongwoo327@gmail.com>
2025-02-28 16:01:34 +0100

    Modified printing to stdout

--- week1/miniProjectFile.sh
+++ week1/miniProjectFile.sh
@@ -2,4 +2,4 @@
 
 find /var/log -type f -name "*.log" -print -exec grep "CRON" {} \;
 tail -F /var/log/system.log | grep "CRON"
-head -n 10 /var/log/system.log
+head -n 10 /var/log/system.log | sudo tee /Users/inyongwoo/MentorshipEarly2025/week1/hello.txt
