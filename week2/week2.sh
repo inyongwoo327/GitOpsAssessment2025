2bc4930d5db01075dba92d3c6ec423d737e4366d
Evan Woo <inyongwoo327@gmail.com>
2025-03-06 03:51:40 +0100

    progress

--- week2/week2.sh
+++ week2/week2.sh
@@ -1,1 +1,10 @@
-grep “session opened for user” /var/log/auth.log | awk '{split $1, a, "T"}'
+echo "First task"
+awk '{split ($1, a, "T"); print a[0]}' /var/log/auth.log | grep “session opened for user”
+echo "\n Second Task"
+wc -l /var/log/auth.log
+echo "\n Third task"
+ps -eu evanwoo | tee allProcesses.log
+echo "\n Fourth Task"
+sed -E 's/^[0-9]{4}-[0-9]{2}-[0-9]{2}T//' /var/log/auth.log | tee auth_clean.log
+sed -E 's/^[0-9]{4}-[0-9]{2}-[0-9]{2} //' /var/log/auth.log | tee cloud-init_clean.log
+diff auth_clean.log cloud-init_clean.log
