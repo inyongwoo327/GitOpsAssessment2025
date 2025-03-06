650483434a8d95dc6dab46a4c079f5ff1e5966c8
Evan Woo <inyongwoo327@gmail.com>
2025-03-06 23:29:24 +0100

    week2 complete

--- week2/week2.sh
+++ week2/week2.sh
@@ -4,7 +4,11 @@ echo "\n Second Task"
 wc -l /var/log/auth.log
 echo "\n Third task"
 ps -eu evanwoo | tee allProcesses.log
+echo "\n or"
+ps aux | awk '{print $1}' | sort | uniq -c | sort -rn
 echo "\n Fourth Task"
 sed -E 's/^[0-9]{4}-[0-9]{2}-[0-9]{2}T//' /var/log/auth.log | tee auth_clean.log
 sed -E 's/^[0-9]{4}-[0-9]{2}-[0-9]{2} //' /var/log/auth.log | tee cloud-init_clean.log
 diff auth_clean.log cloud-init_clean.log
+echo "\n or"
+diff -I '^[^ ]+ ' /var/log/auth.log /var/log/cloud-init.log
