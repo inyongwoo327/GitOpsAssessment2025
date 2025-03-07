99d247382ec081a53a50a0078b82110e1e326d74
Evan Woo <inyongwoo327@gmail.com>
2025-03-07 13:06:44 +0100

    week3 complete

--- week3/week3.sh
+++ week3/week3.sh
@@ -1,5 +1,10 @@
 echo "First task"
 tcpdump
+echo "\n or it should be "
+netstat -tupln
 echo "\n Second Task"
-
+netstat -tupln
+echo "Check out foreign address"
 echo "\n Third Task"
+netstat -tupln
+echo "netstat -n shows numerical address including port."
