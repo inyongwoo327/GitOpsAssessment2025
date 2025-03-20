80f00ff37fc737182301ab37846a5befa2302c16
Evan Woo <inyongwoo327@gmail.com>
2025-03-20 02:08:09 +0100

    Refactored with variable

---
+++ week6-Module2/question1/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.aws_ec2_instance_type
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+variable "aws_ec2_instance_type" {
+  description = "Describes ec2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
